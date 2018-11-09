9ad65916d464fb171a46e52bc46e7291331a514e
paradoxnafi <paradoxnafi@gmail.com>
2018-11-09 20:43:59 +0600

    Building login system

--- windows_only/Login.cpp
+++ windows_only/Login.cpp
@@ -1,14 +1,52 @@
-#include <cstdlib>
 #include <iostream>
-using namespace std;
+#include <fstream>
+#include <string>
+#include <conio.h>
+#include "Login.h"
 
-const char *mypass="null";
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+//using std::getline; // Works fine without it
 
-int main(){
-	char *password=getpass("Enter password: ");
+Login::Login() {
 
-	if(strcmp(password,mypass)==0) cout <<"Correct password!\n";
-	else cout <<"Incorrect password!\n";
+	cout << "Morse Code Translator\n\n";
+	cout << "[1] Login\n";
+	cout << "[2] Signup\n";
+	cout << "\t:";
+	cin >> choice;
 
-	return 0;
+	if(choice == 1) {
+		cout << "Username: ";
+		cin.ignore();
+		getline(cin, USERNAME);
+
+		cout << "Password: ";
+		for(int i = 0; i < 100; i++) {
+			PASSWORD[i] = getch();
+			putch('*');
+			if(PASSWORD[i] == 13) {
+				break;
+			}
+		}
+
+		cout << "\nUsername: " << USERNAME << endl;
+		cout << "Password: " << PASSWORD << endl;
+	}
+
+	if(PASSWORD == admin) {
+		cout << "Granted" << endl;
+	}
 }
+/*
+char s[10] = { 0 };
+    int i;
+    for (i = 0; i < 10;i++) {
+        s[i] = getch(); putch('*');
+        if (s[i] == 13) break;
+    };
+    printf("\nYour pass is %s", s);
+    getchar();
+*/
