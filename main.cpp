e6cc24b35d6341f691f042aa2005cf6c59083489
paradoxnafi <paradoxnafi@gmail.com>
2018-11-09 12:39:24 +0600

    Logo function added

--- main.cpp
+++ main.cpp
@@ -3,17 +3,36 @@
 
 #include <iostream>
 #include <string>
+#include <fstream>
 #include "Login.h"
 #include "Translator.h"
-using namespace std;
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::ifstream;
+
+void logo();
 
 int main()
 {
 //	Translator obj;
 
 //	obj.interface();
+	logo();
 
 	Login login_obj;
 
 	return 0;
+}
+
+// Implementatio of logo function
+void logo() {
+	char c;
+
+	ifstream logo_file("logo");
+
+	while(logo_file.get(c)) {
+		cout << c;
+	}
 }
